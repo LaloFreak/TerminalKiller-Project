ff774404eed296fe04fad7cfd8e58077c302ecc1
Lalo Freak <lalo.gwerh@gmail.com>
2023-02-14 05:00:27 -0300

    Updates

---
+++ precache-manifest.75b62828cab9af85810abe0d19c35d8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b142cb1e9c457ea87c3e",
+    "url": "/static/css/main.a5bee89a.chunk.css"
+  },
+  {
+    "revision": "b142cb1e9c457ea87c3e",
+    "url": "/static/js/main.bd1bf622.chunk.js"
+  },
+  {
+    "revision": "a7a93d1503069d79f58d",
+    "url": "/static/js/runtime~main.629448d9.js"
+  },
+  {
+    "revision": "c7661dc7e829f8961693",
+    "url": "/static/js/2.138eaa1e.chunk.js"
+  },
+  {
+    "revision": "cecf616dbfc830b280dc",
+    "url": "/static/js/3.d7f7bd74.chunk.js"
+  },
+  {
+    "revision": "a50e357f5108b1c0e811332fc9301a9b",
+    "url": "/static/media/logo-icon.a50e357f.png"
+  },
+  {
+    "revision": "1252a14db7080627c0fcc86d7f914bd5",
+    "url": "/static/media/background.1252a14d.png"
+  },
+  {
+    "revision": "31f55fdb62933c76d9b9dbf0081a02fe",
+    "url": "/index.html"
+  }
+];
