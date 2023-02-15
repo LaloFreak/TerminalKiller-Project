39848928d8df4953915d467daf612ceaa2cf3d84
Lalo Freak <lalo.gwerh@gmail.com>
2023-02-14 21:55:10 -0300

    thrash

---
+++ terminalkiller-projects/src/components/LandingPage.jsx
@@ -0,0 +1,9 @@
+import s from './css/LandingPage.module.css'
+
+export const LandingPage = () => {
+    return (
+        <div className={s.contLandingPage}>
+            <h1>Asd</h1>
+        </div>
+    )
+}
