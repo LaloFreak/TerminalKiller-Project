199c3bbc4e51a69bc178e7fbd633272d54afcd4e
Lalo Freak <lalofreak.dev@gmail.com>
2023-06-15 20:44:39 -0400

    styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,15 +4,19 @@ import { Link } from "react-router-dom"
 
 export const Nav = () => {
     return (
-        <div className={s.divNav}>
-            <ul className={s.ulNav}>
-                <Link to="/"><img className={s.terminalkillerLogo} src={terminalkillerLogo} alt="Logo" height="80px" /></Link>
-                <ul className={s.ulMenu}>
-                    <li><Link to="/">Inicio</Link></li>
-                    <li><a href="http://developers.terminalkiller.site" rel="noreferrer">Quiénes somos</a></li>
-                    {/*<li><Link to="/contact">Contacto</Link></li>*/}
-                </ul>
-            </ul>
+        <div className={s.refNav}>
+            <div className={s.navCont}>
+                <div className={s.divNav}>
+                    <ul className={s.ulNav}>
+                        <Link to="/"><img className={s.terminalkillerLogo} src={terminalkillerLogo} alt="Logo" height="80px" /></Link>
+                        <ul className={s.ulMenu}>
+                            <li><Link to="/">Inicio</Link></li>
+                            <li><a href="http://developers.terminalkiller.site" rel="noreferrer">Quiénes somos</a></li>
+                            {/*<li><Link to="/contact">Contacto</Link></li>*/}
+                        </ul>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
