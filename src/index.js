4f2e8a69f3c9c4c3d5880c93f0efb521ebda7bd9
GwerhDev <gwerh.dev@gmail.com>
2024-06-24 10:01:37 -0400

    restructuring

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import './index.css';
+import './styles/root.css';
+import './styles/globals.css';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './tests/reportWebVitals';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
